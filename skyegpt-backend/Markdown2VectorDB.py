00128fd5d2c100ab99a434e04eb31492eeb853ce
pleszr <145920141+pleszr@users.noreply.github.com>
2025-02-04 07:41:03 +0100

    rebased-Refactor Chroma import logic with batch processing
    Replaced document-level imports with a batch-based producer-consumer approach for Chroma data ingestion. Updated related tests, renamed methods for clarity, and added support for new environment variable `CHROMA_BATCH_SIZE` to control batch sizes. These changes improve performance and scalability for bulk data imports.

--- skyegpt-backend/Markdown2VectorDB.py
+++ skyegpt-backend/Markdown2VectorDB.py
@@ -2,7 +2,11 @@ from langchain_text_splitters import MarkdownHeaderTextSplitter
 from pathlib import Path
 from typing import List
 import ChromaSetup
-
+import uuid
+import os
+import time
+import multiprocessing as mp
+from ProcessWrapper import create_process, start_process, join_process
 
 def scan_and_import_markdowns_from_folder(
         collection_name: str,
@@ -10,44 +14,61 @@ def scan_and_import_markdowns_from_folder(
         markdown_split_headers: List[str],
         documentation_source: str
 ):
+    batch_size = int(os.getenv("CHROMA_BATCH_SIZE"))
+    queue = mp.Queue()
+
+    producer_process = create_process(target=chroma_import_producer, args=(
+        folder_path,
+        markdown_split_headers,
+        documentation_source,
+        batch_size,
+        queue
+    ))
+
+    consumer_process = create_process(target=chroma_import_consumer, args=(
+        collection_name,
+        queue
+    ))
+
+    start_time = time.time()
+    start_process(producer_process)
+    start_process(consumer_process)
+
+    join_process(producer_process)
+    queue.put(None)
+
+    join_process(consumer_process)
+
+    number_of_documents = ChromaSetup.number_of_documents_in_collection(collection_name)
+    print(f"Elapsed seconds: {time.time()-start_time:.0f} Record count: {number_of_documents}")
+
+
+def chroma_import_producer(
+        folder_path: str,
+        markdown_split_headers: List[str],
+        documentation_source: str,
+        batch_size: int,
+        queue
+):
     folder = Path(folder_path)
     for file in folder.rglob("*.md"):
-        print(f"Found markdown file: {file}")
-
         with open(file, "r", encoding="utf-8") as opened_file:
             file_content = opened_file.read()
 
-        import_file_to_document(
-            collection_name,
-            markdown_split_headers,
-            Path(file.name).stem,
-            file_content,
-            documentation_source
-        )
+            texts = split_markdown_by_headers(
+                file_content,
+                markdown_split_headers
+            )
 
+            add_text_to_queue(
+                texts,
+                Path(file.name).stem,
+                documentation_source,
+                batch_size,
+                queue
+            )
 
-def import_file_to_document(
-        collection_name: str,
-        markdown_split_headers: List[str],
-        file_name: str,
-        file_content: str,
-        documentation_source
-):
-
-    markdown_header_splits_array = split_markdown_to_header_texts(
-        file_content,
-        markdown_split_headers
-    )
-
-    ChromaSetup.create_document_from_split_texts(
-        collection_name,
-        markdown_header_splits_array,
-        file_name,
-        documentation_source
-    )
-
-
-def split_markdown_to_header_texts(
+def split_markdown_by_headers(
         file_content: str,
         markdown_split_headers: List[str]
 ):
@@ -65,5 +86,56 @@ def split_markdown_to_header_texts(
     string_array = [document.page_content for document in documents_array]
     return string_array
 
+def add_text_to_queue(
+        content_text_array: List[str],
+        file_name: str,
+        documentation_source: str,
+        batch_size: int,
+        queue
+):
+    documents = []
+    metadatas = []
+    ids = []
 
+    for text in content_text_array:
+        documentation_link = ChromaSetup.documentation_link_generator(
+            file_name,
+            documentation_source
+        )
+        metadata = {
+            "file_name": file_name,
+            "documentation_link": documentation_link
+        }
 
+        ids.append(str(uuid.uuid4()))
+        documents.append(text)
+        metadatas.append(metadata)
+
+        if len(ids)>=batch_size:
+            queue.put((documents, metadatas, ids))
+            documents = []
+            metadatas = []
+            ids = []
+
+    if len(ids) > 0:
+        queue.put((documents, metadatas, ids))
+
+def chroma_import_consumer(
+        collection_name,
+        queue
+):
+    collection = ChromaSetup.get_collection_by_name(collection_name)
+    batch_number = 0
+    while True:
+        batch_number += 1
+        print(f"Saving batch: ${batch_number}")
+        batch = queue.get()
+        if batch is None:
+            break
+
+        ChromaSetup.add_to_collection(
+            collection,
+            documents=batch[0],
+            metadatas=batch[1],
+            ids=batch[2]
+        )
--- skyegpt-backend/tests/test_markdown_2_vectordb.py
+++ skyegpt-backend/tests/test_markdown_2_vectordb.py
@@ -1,8 +1,11 @@
-from Markdown2VectorDB import (split_markdown_to_header_texts,
-                               import_file_to_document,
-                               scan_and_import_markdowns_from_folder)
-from unittest.mock import patch, MagicMock, mock_open
 from pathlib import Path
+from Markdown2VectorDB import (split_markdown_by_headers,
+                               scan_and_import_markdowns_from_folder,
+                               chroma_import_producer,
+                               chroma_import_consumer,
+                               add_text_to_queue)
+from unittest.mock import patch, MagicMock, mock_open, call, ANY
+import multiprocessing as mp
 
 markdown_file_content = """
         # Header-11
@@ -25,7 +28,7 @@ def test_split_markdown_to_header_texts_all_headers():
 
     markdown_split_headers = ["#", "##", "###"]
 
-    splits = split_markdown_to_header_texts(
+    splits = split_markdown_by_headers(
         markdown_file_content,
         markdown_split_headers
     )
@@ -40,7 +43,7 @@ def test_split_markdown_to_header_texts_top_two_headers():
 
     markdown_split_headers = ["#", "##"]
 
-    splits = split_markdown_to_header_texts(
+    splits = split_markdown_by_headers(
         markdown_file_content,
         markdown_split_headers
     )
@@ -54,7 +57,7 @@ def test_split_markdown_to_header_texts_top_level_headers():
 
     markdown_split_headers = ["#"]
 
-    splits = split_markdown_to_header_texts(
+    splits = split_markdown_by_headers(
         markdown_file_content,
         markdown_split_headers
     )
@@ -65,80 +68,210 @@ def test_split_markdown_to_header_texts_top_level_headers():
     assert actual_length == expected_length, "Sample document has 2 sections when split at # headers"
 
 
-def test_import_file_to_document_is_calling_functions_with_proper_arguments():
-    collection_name = "test_collection_name"
-    markdown_split_headers = ["#", "##"]
-    file_name = "test_file_name"
-    documentation_source = "test_skye"
-    file_content = "# Test_file_content"
-
-    with patch("Markdown2VectorDB.split_markdown_to_header_texts") as mock_split, \
-            patch("ChromaSetup.create_document_from_split_texts") as mock_create_doc:
-
-        mock_split.return_value = ["test_data", "data_doesnt_need_to_be_valid"]
-
-        import_file_to_document(
-            collection_name,
-            markdown_split_headers,
-            file_name,
-            file_content,
-            documentation_source
-        )
-
-        mock_split.assert_called_once_with(
-            file_content,
-            markdown_split_headers
-        )
-
-        mock_create_doc.assert_called_once_with(
-            collection_name,
-            mock_split.return_value,
-            file_name,
-            documentation_source
-        )
-
-
-def test_scan_and_import_markdowns_from_folder_2_files_success():
+@patch("ChromaSetup.number_of_documents_in_collection")
+@patch("Markdown2VectorDB.join_process")
+@patch("Markdown2VectorDB.start_process")
+@patch("Markdown2VectorDB.create_process")
+def test_scan_and_import_markdowns_from_folder(
+        mock_create_process,
+        mock_start_process,
+        mock_join_process,
+        mock_number_of_documents,
+        monkeypatch
+):
     collection_name = "test_collection"
-    folder_path = "test_folder"
+    folder_path = "test/folder/path"
     markdown_split_headers = ["#", "##"]
-    documentation_source = "test_skye"
+    documentation_source = "test_documentation_source"
 
+    batch_size = 20
+    monkeypatch.setenv("CHROMA_BATCH_SIZE", str(batch_size))
+
+    mock_producer = MagicMock ()
+    mock_consumer = MagicMock()
+    mock_create_process.side_effect = [mock_producer,mock_consumer]
+
+    scan_and_import_markdowns_from_folder(
+        collection_name,
+        folder_path,
+        markdown_split_headers,
+        documentation_source
+    )
+
+    assert mock_create_process.call_count == 2
+    mock_create_process.assert_has_calls([
+        call(target=chroma_import_producer, args=(
+            folder_path,
+            markdown_split_headers,
+            documentation_source,
+            batch_size,
+            ANY
+        )),
+        call(target=chroma_import_consumer, args=(
+            collection_name,
+            ANY
+        ))
+    ])
+    assert mock_start_process.call_count == 2
+    mock_start_process.assert_has_calls([
+        call(mock_producer),
+        call(mock_consumer)
+    ])
+
+    assert mock_join_process.call_count == 2
+    mock_join_process.assert_has_calls([
+        call(mock_producer),
+        call(mock_consumer)
+    ])
+
+    mock_number_of_documents.assert_called_once_with(collection_name)
+
+
+@patch("Markdown2VectorDB.add_text_to_queue")
+@patch("Markdown2VectorDB.split_markdown_by_headers", return_value=["mock text"])
+@patch("builtins.open")
+@patch("pathlib.Path.rglob")
+def test_chroma_import_producer(mock_rglob,
+                                mock_open_file,
+                                mock_split_markdown,
+                                mock_add_text_to_queue):
     mock_files = [MagicMock(spec=Path), MagicMock(spec=Path)]
     mock_files[0].name = "test_file1.md"
     mock_files[1].name = "test_file2.md"
 
-    mock_file = mock_open()
-    mock_file.side_effect = [
-        mock_open(read_data="# Content of test file 1").return_value,
-        mock_open(read_data="# Content of test file 2").return_value
+    mock_rglob.return_value = mock_files
+
+    content_of_test_file_1 = "# Content of test file 1"
+    content_of_test_file_2 = "# Content of test file 2"
+    mock_file1 = mock_open(read_data=content_of_test_file_1).return_value
+    mock_file2 = mock_open(read_data=content_of_test_file_2).return_value
+    mock_open_file.side_effect = [mock_file1, mock_file2]
+
+    folder_path = "test/folder"
+    markdown_split_headers = ["#", "##"]
+    documentation_source = "doc_source"
+    batch_size = 10
+    queue = "mock_queue"
+
+    chroma_import_producer(folder_path,
+                           markdown_split_headers,
+                           documentation_source,
+                           batch_size,
+                           queue)
+
+    expected_split_markdown_by_headers_calls = [
+        call(content_of_test_file_1, markdown_split_headers),
+        call(content_of_test_file_2, markdown_split_headers)
+    ]
+    mock_split_markdown.assert_has_calls(expected_split_markdown_by_headers_calls,
+                                         any_order=True)
+    assert mock_split_markdown.call_count == 2
+
+    expected_add_text_calls = [
+        call(["mock text"], "test_file1", documentation_source, batch_size, queue),
+        call(["mock text"], "test_file2", documentation_source, batch_size, queue)
+    ]
+    mock_add_text_to_queue.assert_has_calls(expected_add_text_calls,
+                                            any_order=True)
+    assert mock_add_text_to_queue.call_count == 2
+
+@patch("ChromaSetup.documentation_link_generator", return_value = "test_docu_link")
+def test_add_test_to_queue(mock_doc_link_generator,
+
+):
+    content_text_array = ["test text 1","test text 2", "test text 3", "test text 4"]
+    file_name = "test_file_name"
+    documentation_source = "test_documentation_source"
+    batch_size = 2
+    queue = mp.Queue()
+
+    add_text_to_queue(content_text_array,
+                      file_name,
+                      documentation_source,
+                      batch_size,
+                      queue)
+
+    assert mock_doc_link_generator.call_count == 4, "documentation_link_generator should be called once per text"
+    mock_doc_link_generator.assert_any_call(file_name, documentation_source)
+
+    items = []
+    while not queue.empty():
+        items.append(queue.get())
+
+    expected_queue_len = len(content_text_array)/batch_size
+    assert len(items) == expected_queue_len
+
+    documents1, metadatas1, ids1 = items[0]
+    assert documents1 == ["test text 1", "test text 2"]
+    assert len(metadatas1) == 2
+    for metadata in metadatas1:
+        assert metadata["file_name"] == file_name
+        assert metadata["documentation_link"] == "test_docu_link"
+    assert len(ids1) == 2
+
+    documents2, metadatas2, ids2 = items[1]
+    assert documents2 == ["test text 3", "test text 4"]
+    assert len(metadatas2) == 2
+    for metadata in metadatas2:
+        assert metadata["file_name"] == file_name
+        assert metadata["documentation_link"] == "test_docu_link", "Incorrect documentation_link in metadata"
+    assert len(ids2) == 2
+
+
+@patch("ChromaSetup.add_to_collection")
+@patch("ChromaSetup.get_collection_by_name")
+def test_chroma_import_consumer(mock_get_collection_by_name,
+                                mock_add_to_collection
+
+):
+    batch1_ids = ["test_id1","test_id2"]
+    batch1_documents = ["test text 1","test text 2"]
+    batch1_metadatas = [
+        {
+            "file_name": "test_file_name1",
+            "documentation_link": "test_docu_link1"
+        },
+        {
+            "file_name": "test_file_name2",
+            "documentation_link": "test_docu_link2"
+        }
     ]
 
-    with patch("Markdown2VectorDB.Path.rglob", return_value=mock_files), \
-            patch("builtins.open", mock_file), \
-            patch("Markdown2VectorDB.import_file_to_document") as mock_import:
+    batch2_ids = ["test_id3","test_id4"]
+    batch2_documents = ["test text 3","test text 4"]
+    batch2_metadatas = [
+        {
+            "file_name": "test_file_name3",
+            "documentation_link": "test_docu_link3"
+        },
+        {
+            "file_name": "test_file_name4",
+            "documentation_link": "test_docu_link4"
+        }
+    ]
 
-        scan_and_import_markdowns_from_folder(
-            collection_name,
-            folder_path,
-            markdown_split_headers,
-            documentation_source
-        )
+    collection_name = "test_collection_name"
+    queue = mp.Queue()
 
-        mock_import.assert_any_call(
-            collection_name,
-            markdown_split_headers,
-            "test_file1",
-            "# Content of test file 1",
-            documentation_source
-        )
+    mock_collection = MagicMock()
+    mock_get_collection_by_name.return_value = mock_collection
 
-        mock_import.assert_any_call(
-            collection_name,
-            markdown_split_headers,
-            "test_file2",
-            "# Content of test file 2",
-            documentation_source
-        )
+    queue.put((batch1_documents, batch1_metadatas, batch1_ids))
+    queue.put((batch2_documents, batch2_metadatas, batch2_ids))
+    queue.put(None)
 
-    assert mock_import.call_count == 2, "Expected import_file_to_document to be called twice, once for each file."
+    chroma_import_consumer(
+        collection_name,
+        queue
+    )
+
+
+    expected_add_to_collection_calls = [
+        call(mock_collection, documents=batch1_documents, metadatas=batch1_metadatas, ids=batch1_ids),
+        call(mock_collection, documents=batch2_documents, metadatas=batch2_metadatas, ids=batch2_ids)
+    ]
+    mock_add_to_collection.assert_has_calls(expected_add_to_collection_calls,any_order=True
+
+    )
+
+
